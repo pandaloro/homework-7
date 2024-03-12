364fddf12364a37ef8b790c7c140549d03ae911c
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-03-12 16:59:55 -0400

    Update PostController.php

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 public function posts()
     {
     $userModel = new User();
-    $template = $this->twig->load('posts/posts.twig');
+    $template = $this->twig->load('posts.twig');
     $usersData = [
         'users' => $userModel->getAllUsers(),
     ];
