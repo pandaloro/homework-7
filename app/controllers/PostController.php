272c7dd0f7134733ef39d60315d306d2b657d9fc
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-03-12 16:57:24 -0400

    Update PostController.php

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -7,6 +7,14 @@ class PostController extends Controller
 //todo make a method to return some posts, post objects should come from the post model class
 //also need to make a twig template to show the posts
 //an example is in app/controllers/UsersController
+public function posts()
+    {
+    $userModel = new User();
+    $template = $this->twig->load('posts/posts.twig');
+    $usersData = [
+        'users' => $userModel->getAllUsers(),
+    ];
 
-
+    echo $template->render($usersData);
+}
 }
