f44fa585ac3faf82e2f882585cf2c0e93f335525
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-03-18 19:03:41 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,18 +11,33 @@ use app\controllers\UserController;
 use app\controllers\PostController;
 
 
-$url = $_SERVER["REQUEST_URI"];
-
+$url = strtok($_SERVER["REQUEST_URI"], '?');
 //todo add a switch statement router to route based on the url
 //if it is "/posts" return an array of posts via the post controller
 //if it is "/" return the homepage view from the main controller
 //if it is something else return a 404 view from the main controller
-switch ($url) {
+$mainController = new MainController();
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $url == '/posts') {
+   echo 'request method is post';
+   $postController = new PostController();
+       $postController->savePost();
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET' && $url == '/posts')
+{
+    $postController = new PostController();
+    $postController->postsView();
+} else
+{
+    switch ($url) {
     case '/posts':
-        // Return an array of posts via the PostController
-        $postController->posts();
+        { 
+            $postController = new PostController();
+            // Return an array of posts via the PostController
+            $postController->postsView();  
+        }
         break;
-    case '/':
+    case '/': 
         // Return the homepage view from the MainController
         $mainController->homepage();
         break;
@@ -30,4 +45,5 @@ switch ($url) {
         // Return a 404 view from the MainController
         $mainController->notFound();
         break;
+}
 }
