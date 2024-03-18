68d446a1f6fa359751735b1f6947d7ebb91ff2bc
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-03-18 19:00:54 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,24 @@ namespace app\models;
 
 class Post
 {
+    private $postData = [
+        [
+            'id' => '1',
+            'name' => 'pinecone'
+        ],
+        [
+            'id' => '2',
+            'name' => 'nathan'
+        ]
+    ];
 
+    public function getAllPosts() {
+        //in future this will come from the database
+        return $this->postData;
+    }
+
+    public function savePost($newPostData) {
+        // Assuming $newPostData is an associative array with 'id' and 'name' keys
+        $this->postData[] = $newPostData;
+    }
 }
